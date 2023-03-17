7adf7badb60f1912147986c98c67a900a1855423
JuanPalms <palmerosjuanfrancisco@gmail.com>
2023-03-16 22:06:23 -0600

    Changes key for S3 bucket

--- etl/load.py
+++ etl/load.py
@@ -13,11 +13,11 @@ session = boto3.Session(profile_name='datascientist')
 s3 = boto3.client('s3')
 
 # Crea un nuevo Bucket
-BUCKET_NAME = "itam-analytics-palmeros-1"
+BUCKET_NAME = "itam-analytics-palms"
 s3.create_bucket(Bucket=BUCKET_NAME)
 
 # Upload los dos archivos a S3
 s3.upload_file(Filename=os.path.join(config_f["data_directory"]+config_f["raw_data"],"simpsons_episode_guests.csv"),
-               Bucket=BUCKET_NAME, Key="simpsons/raw/seasons/simpsons_episodios_totales.csv")
+               Bucket=BUCKET_NAME, Key="simpsons/raw/seasons/simpsons_guests.csv")
 s3.upload_file(Filename=os.path.join(config_f["data_directory"]+config_f["raw_data"],"simpsons_episodes_totals.csv"),
-               Bucket=BUCKET_NAME, Key="simpsons/raw/guests/simpsons_invitados.csv")
+               Bucket=BUCKET_NAME, Key="simpsons/raw/guests/simpsons_episodes.csv")
